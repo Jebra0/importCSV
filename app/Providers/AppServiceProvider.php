c089fc5249c3384aa26494465c3f7818f2019a97
mohammed <mohammed01110834150@gmail.com>
2024-12-19 16:28:01 +0200

    jobs events

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use App\Jobs\ImportCsvJob;
+use Illuminate\Foundation\Application;
+use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Queue\Events\QueueBusy;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +26,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // max jobs count in the queue event
+        Event::listen(function (QueueBusy $event) {
+            Log::alert('queue jobs: ',  [
+                'queue::::: ' => $event->queue,
+                'size::::: ' => $event->size
+            ]);
+        });
+
+        ////////////////////////////////////////
+
+        Queue::failing(function (JobFailed $event) {
+            Log::error('Job Failed', [
+                // the connection job file
+                'connection' => $event->connectionName,
+                'job' => $event->job->getName(),
+                'exception' => $event->exception->getMessage(),
+            ]);
+
+            //notify the user or any
+        });
     }
 }
